e4aa09a4a4af362285462bc6021e03c22ffc1f3e
will-me-me <willykaria777@gmail.com>
2023-10-05 11:50:53 -0700

    issue fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-
-def main():
-    d_name = dir(hidden_4)
-
-    for i in d_name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
-
 if __name__ == "__main__":
-    main()
+    for item in dir(hidden_4):
+        if item[0] == "_":
+            continue
+        print(item)
