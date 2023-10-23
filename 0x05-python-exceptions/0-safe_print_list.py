688409a2882be65d3a92d4916106df4993b136f4
will-me-me <willykaria777@gmail.com>
2023-10-23 06:36:51 -0700

    fixed code error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
-    print()
-    return(total)
+    try:
+        i = 0
+        for j in range(x):
+            print("{}".format(my_list[i]), end="")
+            i = i + 1
+    except _:
+        pass
+    finally:
+        print()
+        return i
