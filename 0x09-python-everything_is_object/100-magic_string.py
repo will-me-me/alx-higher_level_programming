c205673a67cc1ffd1a5a4937125154bcfcc06529
will-me-me <willykaria777@gmail.com>
2023-10-31 13:39:31 -0700

    updated the functionality

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-magic_string = __import__('100-magic_string').magic_string
-
-for i in range(10):
-    print(magic_string())
+def magic_string(arr=[]):
+    arr.append("BestSchool")
+    return ", ".join(arr)
